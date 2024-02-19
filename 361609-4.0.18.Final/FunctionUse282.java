6a614b36422328a8f82ca8428ed38d304db64ac0
duzhg56980 <guokey1996@gmail.com>
2024-02-19 12:02:45 +0800

    init commit

---
+++ 361609-4.0.18.Final/FunctionUse282.java
@@ -0,0 +1,6 @@
+import io.netty.handler.ssl.NotSslRecordException;
+public class FunctionUse282 {
+public void funcUse() {
+NotSslRecordException notsslrecordexception = new NotSslRecordException();
+}
+}
---
+++ 361609-4.0.18.Final/FunctionUse294.java
@@ -0,0 +1,6 @@
+import io.netty.handler.ssl.SslHandshakeCompletionEvent;
+public class FunctionUse294 {
+public void funcUse() {
+SslHandshakeCompletionEvent sslhandshakecompletionevent = new SslHandshakeCompletionEvent();
+}
+}
